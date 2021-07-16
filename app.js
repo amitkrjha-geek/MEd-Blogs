f51a00d2bd7e2dad3ef3323bcdff569e743273d5
Dev Kumar Seth <devkumar.seth.cd.mat19@itbhu.ac.in>
2021-07-17 01:04:36 +0530

    main site added

--- app.js
+++ app.js
@@ -42,12 +42,48 @@ const contactsSchema = {
 }
 const Contact = mongoose.model('Contact', contactsSchema);
 
+const mainContactsSchema = {
+    name: String,
+    email: String,
+    subject: String,
+    msg: String,
+    date: {},
+}
+const Contact = mongoose.model('Contact', mainContactsSchema);
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 
 
-app.get("/", function(req, res) {
+
+
+app.get('/', function(req, res) {
+    res.render('index');
+})
+app.post('/', function(req, res) {
+    let nDate = new Date();
+    let contact = new Contact({
+        name: req.body.name,
+        email: req.body.email,
+        subject: req.body.subject,
+        msg: req.body.msg,
+        date: nDate,
+
+    })
+    contact.save();
+    Contact.find({}, function(err, contact) {
+        console.log(contact);
+    })
+    res.redirect("/");
+})
+
+
+app.get("/blog-home", function(req, res) {
     BlogPost.find({}, function(err, blogPosts) {
-        res.render("index", { blogPosts: blogPosts });
+        res.render("blog-index", { blogPosts: blogPosts });
     })
 })
 
